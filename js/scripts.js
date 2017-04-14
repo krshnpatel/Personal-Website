65ae6b74a113a1ec1f86c9f25aa89ff9782b983f
Krishan Patel <krishanpatel09@gmail.com>
2017-04-14 01:33:03 -0400

    Added Navbar, Carousel, Footer

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,35 @@
 $(document).ready(function () {
     // Initialization
-    $('.parallax').parallax();
+    $('.carousel.carousel-slider').carousel({ fullWidth: true });
+    $('#mainCarousel').css('height', $('body').height() - $('footer').height() - $('nav').height());
+
+    // Event Listeners
+    $('#linkOne').click(function () {
+        $('.carousel').carousel('set', 0);
+    });
+
+    $('#linkTwo').click(function () {
+        $('.carousel').carousel('set', 1);
+    });
+
+    $('#linkThree').click(function () {
+        $('.carousel').carousel('set', 2);
+    });
+
+    $('#linkFour').click(function () {
+        $('.carousel').carousel('set', 3);
+    });
+
+    $('#navbarTitle').hover(
+        function () {
+            $('.carousel').hide();
+            $('#navbarTitle').removeClass("thin");
+            $('#navbarTitle').addClass("light");
+        },
+        function () {
+            $('.carousel').show();
+            $('#navbarTitle').addClass("thin");
+            $('#navbarTitle').removeClass("light");
+        }
+    );
 });
